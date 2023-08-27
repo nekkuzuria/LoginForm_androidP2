e05e4406236e9bf390ee530be5bd8a0608793a37
nekkuzuria <amandafarliana10@gmail.com>
2023-08-27 13:41:38 +0700

    update kotlin file

--- app/src/main/java/com/example/loginform_androidp2/MainActivity.kt
+++ app/src/main/java/com/example/loginform_androidp2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.loginform_androidp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.*
 import com.example.loginform_androidp2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         with(binding){
+            buttonLogin.setOnClickListener(){
+                if(usernameEditText.text.isNullOrBlank()||passwordEditText.text.isNullOrBlank()){
+                    Toast.makeText(this@MainActivity, "Please fill Username and Password", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    val username = "amanda"
+                    val password = "terpal"
+
+                    if(usernameEditText.text.toString().equals(username) && passwordEditText.text.toString().equals(password)){
+                        Toast.makeText(this@MainActivity, "Login Success", Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Toast.makeText(this@MainActivity, "Login Failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
 
         }
 
